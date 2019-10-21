ec069a10bdb30bcf699d893bb09503813a6fc584
Danilo Horta <danilo.horta@pm.me>
2019-10-21 17:39:15 +0100

    viterbi del state

--- nmm/_hmm.py
+++ nmm/_hmm.py
@@ -1,10 +1,10 @@
-from math import exp, isinf, inf, isnan
+from math import isnan
 from ._string import make_sure_bytes
 from ._path import Path
 from ._log import LOG
 from ._state import State
 from ._alphabet import Alphabet
-import functools
+from typing import Dict
 
 from ._ffi import ffi, lib
 
@@ -13,7 +13,7 @@ class HMM:
     def __init__(self, alphabet: Alphabet):
         self._alphabet = alphabet
         self._hmm = lib.imm_hmm_create(self._alphabet.cdata)
-        self._states = {}
+        self._states: Dict[ffi.CData, State] = {}
 
     def __del__(self):
         if self._hmm != ffi.NULL:
@@ -75,32 +75,15 @@ class HMM:
             raise ValueError("Could not add state %s.", state)
         self._states[state.cdata] = state
 
-    def rename_state(self, old_name: str, new_name: str):
-        if old_name not in self._states:
-            raise ValueError(f"State name `{old_name}` does not exist.")
+    def del_state(self, state: State):
+        if state.cdata not in self._states:
+            raise ValueError(f"State {state} not found.")
 
-        if new_name in self._states:
-            raise ValueError(f"State name `{new_name}` already exists.")
+        err: int = lib.imm_hmm_del_state(self._hmm, state.cdata)
+        if err != 0:
+            raise ValueError(f"Could not delete state {state}.")
 
-        self._states[new_name] = self._states.pop(old_name)
-        self._states[new_name].name = new_name
-        self._init_logps[new_name] = self._init_logps.pop(old_name)
-
-        for k, v in self._trans.items():
-            if old_name in v:
-                v[new_name] = v.pop(old_name)
-        self._trans[new_name] = self._trans.pop(old_name)
-
-    def delete_state(self, name):
-        if name not in self._states:
-            raise ValueError(f"State name `{name}` does not exist.")
-
-        del self._states[name]
-        del self._init_logps[name]
-        del self._trans[name]
-        for v in self._trans.values():
-            if name in v:
-                del v[name]
+        del self._states[state.cdata]
 
     def normalize(self):
         err: int = lib.imm_hmm_normalize(self._hmm)
@@ -114,187 +97,9 @@ class HMM:
             raise ValueError("Could not calculate the likelihood.")
         return lprob
 
-    # def draw(self, filepath, emissions=0, init_prob=True, digits=3, view=False):
-    #     from graphviz import Digraph
-
-    #     graph = Digraph()
-
-    #     for state in self._states.values():
-    #         shape = "circle"
-
-    #         if init_prob:
-    #             p = self.init_prob(state.name, log_space=False)
-    #             p = round(p, digits)
-    #             if p > 0:
-    #                 state_label = f"{state.name}: {p}"
-    #             else:
-    #                 state_label = f"{state.name}"
-    #         else:
-    #             state_label = f"{state.name}"
-
-    #         if emissions > 0:
-    #             emission = state.emission(log_space=False)
-    #             emission = emission[:emissions]
-    #             label = _format_emission_table(emission, state_label, digits)
-    #         else:
-    #             label = state_label
-
-    #         graph.node(state.name, label, shape=shape)
-
-    #     for state0, trans in self._trans.items():
-    #         for state1, logp in trans.items():
-    #             p = exp(logp)
-    #             p = round(p, digits)
-    #             if p > 0:
-    #                 graph.edge(state0, state1, label=f"{p}")
-
-    #     graph.render(filepath, view=view)
-
-    def viterbi(self, seq: str, end_state: str, log_space: bool = False):
-        self._check_mute_cycle()
-        self._vcache = {}
-        max_logp = LOG(0.0)
-        best_path = []
-
-        end_state = self._states[end_state]
-        for ft in range(end_state.min_len, end_state.max_len + 1):
-            tup = self._viterbi(seq, end_state, ft)
-            if tup[0] > max_logp:
-                max_logp = tup[0]
-                best_path = tup[1] + [(end_state, ft)]
-
-        best_path = [(qt.name, ft) for qt, ft in best_path]
-        if log_space:
-            return max_logp, best_path
-        return exp(max_logp), best_path
-
-    @functools.lru_cache(maxsize=65536)
-    def _viterbi(self, seq: str, qt: str, ft: int):
-        max_logp = LOG(0.0)
-        best_path = []
-        if ft > len(seq):
-            return max_logp, best_path
-
-        emission_prob = qt.prob(seq[len(seq) - ft :], True)
-        if emission_prob <= max_logp:
-            return max_logp, best_path
-
-        seq_end = len(seq) - ft
-        prefix = seq[:seq_end]
-        for qt_1 in self._states.values():
-            if qt_1.min_len > len(prefix):
-                continue
-
-            T = self.trans(qt_1.name, qt.name, True)
-            if T + emission_prob <= max_logp:
-                continue
-
-            for ft_1 in range(qt_1.min_len, qt_1.max_len + 1):
-                tup = self._viterbi(prefix, qt_1, ft_1)
-                tup = (tup[0] + T + emission_prob, tup[1] + [(qt_1, ft_1)])
-
-                if tup[0] > max_logp:
-                    max_logp = tup[0]
-                    best_path = tup[1]
-
-        if len(seq) - ft == 0:
-            v = emission_prob + self.init_prob(qt.name, True)
-            if v > max_logp:
-                max_logp = v
-                best_path = []
-
-        return max_logp, best_path
-
-    def _draw_initial_state(self, random):
-        names = self._init_logps.keys()
-        probs = [exp(v) for v in self._init_logps.values()]
-        name = random.choice(list(names), p=probs)
-        return self._states[name]
-
-    def _transition(self, state: State, random):
-        trans = self._trans[state.name]
-        names = trans.keys()
-        probs = [exp(v) for v in trans.values()]
-        name = random.choice(list(names), p=probs)
-        return self._states[name]
-
-    def _normalize_trans(self):
-        from scipy.special import logsumexp
-
-        names = self._states.keys()
-        nstates = len(names)
-        for a in names:
-            logprobs = list(self._trans[a].values())
-
-            if len(logprobs) == 0:
-                continue
-                # for b in names:
-                #     self._trans[a][b] = -LOG(nstates)
-            # else:
-            logprob_norm = logsumexp(logprobs)
-            if isinf(logprob_norm):
-                for b in names:
-                    self._trans[a][b] = -LOG(nstates)
-            else:
-                for b in self._trans[a].keys():
-                    self._trans[a][b] -= logprob_norm
-
-    def _normalize_init_logps(self):
-        from scipy.special import logsumexp
-
-        logprobs = list(self._init_logps.values())
-        names = self._states.keys()
-        nstates = len(names)
-
-        logp_norm = logsumexp(logprobs)
-        if isinf(logp_norm):
-            for a in names:
-                self._init_logps[a] = -LOG(nstates)
-        else:
-            for a in self._init_logps.keys():
-                self._init_logps[a] -= logp_norm
-
-    def _assure_states_exist(self, states):
-        for state in states:
-            if state not in self._states:
-                raise ValueError(f"State `{state}` does not exist.")
-
-    def _check_mute_cycle(self):
-        state_marks = {name: "initial" for name in self._states}
-        for state_name in self._states:
-            self._mute_cycle_visit(state_name, state_marks)
-
-    def _mute_cycle_visit(self, state: str, state_marks):
-        if self._states[state].min_len > 0:
-            return
-        if state_marks[state] == "permament":
-            return
-        if state_marks[state] == "temporary":
-            raise ValueError("Mute cycles are not allowed.")
-
-        state_marks[state] = "temporary"
-        for dst in self._trans[state]:
-
-            if self._trans[state][dst] > float("-inf"):
-                self._mute_cycle_visit(dst, state_marks)
-
-        state_marks[state] = "permanent"
-
-
-def _format_emission_table(emission, name, digits):
-    rows = ""
-    for row in emission:
-        seq = row[0]
-        p = round(row[1], digits)
-        if p == 0.0:
-            break
-        rows += f"<TR><TD>{seq}</TD><TD>{p}</TD></TR>"
-
-    tbl_fmt = "BORDER='0' CELLBORDER='1' "
-    tbl_fmt += "CELLSPACING='0' CELLPADDING='4'"
-    tbl_str = f"<<TABLE {tbl_fmt}>"
-    tbl_str += f"<TR><TD COLSPAN='2'>{name}</TD></TR>"
-    tbl_str += rows
-    tbl_str += "</TABLE>>"
-
-    return tbl_str
+    def viterbi(self, seq: str, end_state: State):
+        seq = make_sure_bytes(seq)
+        lprob: float = lib.imm_hmm_viterbi(self._hmm, seq, end_state.cdata)
+        if isnan(lprob):
+            raise ValueError("Could not calculate the viterbi score.")
+        return lprob
--- nmm/test/test_hmm.py
+++ nmm/test/test_hmm.py
@@ -1,7 +1,6 @@
-from math import isinf, nan
 import pytest
-from numpy.testing import assert_allclose, assert_equal
-from nmm import MuteState, NormalState, TableState, FrameState, Alphabet
+from numpy.testing import assert_allclose
+from nmm import MuteState, NormalState, TableState, Alphabet
 from nmm import HMM, LOG, Path
 
 
@@ -224,275 +223,139 @@ def test_hmm_lik_3():
     assert_allclose(p, LOG(0.5))
 
 
-# def test_hmm_viterbi_1():
-#     alphabet = "ACGU"
-
-#     hmm = HMM(alphabet)
-#     start_state = MuteState("S", alphabet)
-#     hmm.add_state(start_state, LOG(1.0))
-
-#     end_state = MuteState("E", alphabet)
-#     hmm.add_state(end_state, LOG(0.0))
-
-#     M1 = NormalState("M1", {"A": LOG(0.8), "C": LOG(0.2), "G": LOG(0.0), "U": LOG(0.0)})
-#     hmm.add_state(M1, LOG(0.0))
-
-#     M2 = NormalState("M2", {"A": LOG(0.4), "C": LOG(0.6), "G": LOG(0.0), "U": LOG(0.6)})
-#     hmm.add_state(M2, LOG(0.0))
-
-#     hmm.set_trans("S", "M1", LOG(1.0))
-#     hmm.set_trans("M1", "M2", LOG(1.0))
-#     hmm.set_trans("M2", "E", LOG(1.0))
-#     hmm.normalize()
-
-#     lik, path = hmm.viterbi("AC", "E")
-#     assert_allclose(lik, 0.3)
-#     assert [p[0] for p in path] == ["S", "M1", "M2", "E"]
-#     assert path[0][1] == 0
-#     assert path[1][1] == 1
-#     assert path[2][1] == 1
-#     assert path[3][1] == 0
-
-#     lik = hmm.viterbi("AC", "E", True)[0]
-#     assert_allclose(lik, LOG(0.3))
-
-#     p = hmm.likelihood("AC", [("S", 0), ("M1", 1), ("M2", 1), ("E", 0)])
-#     assert_allclose(p, 0.3)
-#     logp = hmm.likelihood("AC", [("S", 0), ("M1", 1), ("M2", 1), ("E", 0)], True)
-#     assert_allclose(logp, LOG(0.3))
-
-
-# def test_hmm_viterbi_2():
-#     alphabet = "AC"
-
-#     hmm = HMM(alphabet)
-#     start_state = MuteState("S", alphabet)
-#     hmm.add_state(start_state, LOG(1.0))
-
-#     end_state = MuteState("E", alphabet)
-#     hmm.add_state(end_state, LOG(0.0))
-
-#     M1 = NormalState("M1", {"A": LOG(0.8), "C": LOG(0.2)})
-#     hmm.add_state(M1, LOG(0.0))
-
-#     M2 = NormalState("M2", {"A": LOG(0.4), "C": LOG(0.6)})
-#     hmm.add_state(M2, LOG(0.0))
-
-#     hmm.set_trans("S", "M1", LOG(1.0))
-#     hmm.set_trans("M1", "M2", LOG(1.0))
-#     hmm.set_trans("M2", "E", LOG(1.0))
-#     hmm.normalize()
-
-#     lik, path = hmm.viterbi("AC", "E")
-#     assert_allclose(lik, 0.48)
-#     assert [p[0] for p in path] == ["S", "M1", "M2", "E"]
-#     assert list(p[1] for p in path) == [0, 1, 1, 0]
-#     p = hmm.likelihood("AC", [("S", 0), ("M1", 1), ("M2", 1), ("E", 0)])
-#     assert_allclose(p, 0.48)
-
-#     lik, path = hmm.viterbi("AA", "E")
-#     assert_allclose(lik, 0.32)
-#     assert [p[0] for p in path] == ["S", "M1", "M2", "E"]
-#     assert list(p[1] for p in path) == [0, 1, 1, 0]
-#     p = hmm.likelihood("AA", [("S", 0), ("M1", 1), ("M2", 1), ("E", 0)])
-#     assert_allclose(p, 0.32)
-
-#     lik, path = hmm.viterbi("CA", "E")
-#     assert_allclose(lik, 0.08)
-#     assert [p[0] for p in path] == ["S", "M1", "M2", "E"]
-#     assert list(p[1] for p in path) == [0, 1, 1, 0]
-#     p = hmm.likelihood("CA", [("S", 0), ("M1", 1), ("M2", 1), ("E", 0)])
-#     assert_allclose(p, 0.08)
-
-#     lik, path = hmm.viterbi("CC", "E")
-#     assert_allclose(lik, 0.12)
-#     assert [p[0] for p in path] == ["S", "M1", "M2", "E"]
-#     assert list(p[1] for p in path) == [0, 1, 1, 0]
-#     p = hmm.likelihood("CC", [("S", 0), ("M1", 1), ("M2", 1), ("E", 0)])
-#     assert_allclose(p, 0.12)
-
-#     hmm.set_trans("M1", "E", LOG(1.0))
-#     hmm.normalize()
-
-#     lik, path = hmm.viterbi("AC", "E")
-#     assert_allclose(lik, 0.48)
-#     assert [p[0] for p in path] == ["S", "M1", "M2", "E"]
-#     assert list(p[1] for p in path) == [0, 1, 1, 0]
-#     p = hmm.likelihood("AC", [("S", 0), ("M1", 1), ("M2", 1), ("E", 0)])
-#     assert_allclose(p, 0.24)
-
-#     lik, path = hmm.viterbi("AA", "E")
-#     assert_allclose(lik, 0.32)
-#     assert [p[0] for p in path] == ["S", "M1", "M2", "E"]
-#     assert list(p[1] for p in path) == [0, 1, 1, 0]
-#     p = hmm.likelihood("AA", [("S", 0), ("M1", 1), ("M2", 1), ("E", 0)])
-#     assert_allclose(p, 0.16)
+def test_hmm_viterbi_1():
+    alphabet = Alphabet("ACGU")
+    hmm = HMM(alphabet)
 
-#     lik, path = hmm.viterbi("CA", "E")
-#     assert_allclose(lik, 0.08)
-#     assert [p[0] for p in path] == ["S", "M1", "M2", "E"]
-#     assert list(p[1] for p in path) == [0, 1, 1, 0]
-#     p = hmm.likelihood("CA", [("S", 0), ("M1", 1), ("M2", 1), ("E", 0)])
-#     assert_allclose(p, 0.04)
-
-#     lik, path = hmm.viterbi("CC", "E")
-#     assert_allclose(lik, 0.12)
-#     assert [p[0] for p in path] == ["S", "M1", "M2", "E"]
-#     assert list(p[1] for p in path) == [0, 1, 1, 0]
-#     p = hmm.likelihood("CC", [("S", 0), ("M1", 1), ("M2", 1), ("E", 0)])
-#     assert_allclose(p, 0.06)
-
-
-# def test_hmm_viterbi_3():
-#     alphabet = "AC"
-
-#     hmm = HMM(alphabet)
-#     start_state = MuteState("S", alphabet)
-#     hmm.add_state(start_state, LOG(1.0))
+    S = MuteState("S", alphabet)
+    hmm.add_state(S, LOG(1.0))
 
-#     end_state = MuteState("E", alphabet)
-#     hmm.add_state(end_state, LOG(0.0))
+    E = MuteState("E", alphabet)
+    hmm.add_state(E, LOG(0.0))
 
-#     M1 = NormalState("M1", {"A": LOG(0.8), "C": LOG(0.2)})
-#     hmm.add_state(M1, LOG(0.0))
-
-#     D1 = MuteState("D1", alphabet)
-#     hmm.add_state(D1, LOG(0.0))
-
-#     M2 = NormalState("M2", {"A": LOG(0.4), "C": LOG(0.6)})
-#     hmm.add_state(M2, LOG(0.0))
-
-#     D2 = MuteState("D2", alphabet)
-#     hmm.add_state(D2, LOG(0.0))
+    M1 = NormalState("M1", alphabet, [LOG(0.8), LOG(0.2), LOG(0.0), LOG(0.0)])
+    hmm.add_state(M1, LOG(0.0))
 
-#     hmm.set_trans("S", "M1", LOG(0.8))
-#     hmm.set_trans("S", "D1", LOG(0.2))
-
-#     hmm.set_trans("M1", "M2", LOG(0.8))
-#     hmm.set_trans("M1", "D2", LOG(0.2))
-
-#     hmm.set_trans("D1", "D2", LOG(0.2))
-#     hmm.set_trans("D1", "M2", LOG(0.8))
-
-#     hmm.set_trans("D2", "E", LOG(1.0))
-#     hmm.set_trans("M2", "E", LOG(1.0))
-#     hmm.normalize()
+    M2 = NormalState("M2", alphabet, [LOG(0.4), LOG(0.6), LOG(0.0), LOG(0.6)])
+    M2.normalize()
+    hmm.add_state(M2, LOG(0.0))
 
-#     lik, path = hmm.viterbi("AC", "E")
-#     assert_allclose(lik, 0.3072)
-#     assert [p[0] for p in path] == ["S", "M1", "M2", "E"]
-#     assert list(p[1] for p in path) == [0, 1, 1, 0]
-#     p = hmm.likelihood("AC", [("S", 0), ("M1", 1), ("M2", 1), ("E", 0)])
-#     assert_allclose(p, 0.3072)
+    hmm.set_trans(S, M1, LOG(1.0))
+    hmm.set_trans(M1, M2, LOG(1.0))
+    hmm.set_trans(M2, E, LOG(1.0))
+    hmm.set_trans(E, E, LOG(1.0))
+    hmm.normalize()
 
-#     lik, path = hmm.viterbi("AA", "E")
-#     assert_allclose(lik, 0.2048)
-#     assert [p[0] for p in path] == ["S", "M1", "M2", "E"]
-#     assert list(p[1] for p in path) == [0, 1, 1, 0]
-#     p = hmm.likelihood("AA", [("S", 0), ("M1", 1), ("M2", 1), ("E", 0)])
-#     assert_allclose(p, 0.2048)
+    with pytest.raises(ValueError):
+        hmm.viterbi("AC", E)
 
-#     lik, path = hmm.viterbi("A", "E")
-#     assert_allclose(lik, 0.128)
-#     assert [p[0] for p in path] == ["S", "M1", "D2", "E"]
-#     assert list(p[1] for p in path) == [0, 1, 0, 0]
-#     p = hmm.likelihood("A", [("S", 0), ("M1", 1), ("D2", 0), ("E", 0)])
-#     assert_allclose(p, 0.128)
+    hmm.set_trans(E, E, LOG(0.0))
+    assert_allclose(hmm.trans(E, E), LOG(0.0))
+    assert_allclose(hmm.trans(S, S), LOG(0.0))
+    assert_allclose(hmm.trans(S, E), LOG(0.0))
+    assert_allclose(hmm.trans(E, S), LOG(0.0))
 
+    lik = hmm.viterbi("AC", E)
+    assert_allclose(lik, LOG(0.3))
 
-# def test_hmm_draw(tmp_path):
-#     hmm = _create_hmm()
 
-#     base_emission = {"A": LOG(0.5), "C": LOG(0.5)}
-#     codon_emission = {"ACC": LOG(0.8), "AAA": LOG(0.2)}
-#     epsilon = 0.1
-#     M0 = FrameState("M0", base_emission, codon_emission, epsilon)
-#     hmm.add_state(M0, LOG(1.0))
-#     hmm.set_trans("M0", "E", LOG(1.0))
-#     hmm.normalize()
+def test_hmm_viterbi_2():
+    alphabet = Alphabet("AC")
 
-#     hmm.draw(tmp_path / "test.pdf", emissions=5, init_prob=True)
-#     hmm.draw(tmp_path / "test.pdf", emissions=3, init_prob=False)
-#     hmm.draw(tmp_path / "test.pdf", emissions=0, init_prob=True)
-#     hmm.draw(tmp_path / "test.pdf", emissions=0, init_prob=False)
-#     hmm.draw(tmp_path / "test.pdf", emissions=50, init_prob=True)
+    hmm = HMM(alphabet)
+    S = MuteState("S", alphabet)
+    hmm.add_state(S, LOG(1.0))
 
+    E = MuteState("E", alphabet)
+    hmm.add_state(E, LOG(0.0))
 
-# def test_hmm_rename_state():
-#     hmm = _create_hmm()
+    M1 = NormalState("M1", alphabet, [LOG(0.8), LOG(0.2)])
+    hmm.add_state(M1, LOG(0.0))
 
-#     with pytest.raises(ValueError):
-#         hmm.rename_state("DD", "D1")
+    M2 = NormalState("M2", alphabet, [LOG(0.4), LOG(0.6)])
+    hmm.add_state(M2, LOG(0.0))
 
-#     with pytest.raises(ValueError):
-#         hmm.rename_state("E", "M1")
+    hmm.set_trans(S, M1, LOG(1.0))
+    hmm.set_trans(M1, M2, LOG(1.0))
+    hmm.set_trans(M2, E, LOG(1.0))
+    hmm.set_trans(E, E, LOG(1.0))
+    hmm.normalize()
+    hmm.set_trans(E, E, LOG(0.0))
 
-#     p = hmm.trans("D1", "D2")
-#     hmm.rename_state("S", "B")
-#     hmm.rename_state("D2", "DD")
-#     assert "B" in hmm.states
-#     assert "S" not in hmm.states
-#     assert "DD" in hmm.states
-#     assert "D2" not in hmm.states
-#     assert hmm.trans("D1", "DD") == p
+    lik = hmm.viterbi("AC", E)
+    assert_allclose(lik, LOG(0.48))
 
+    lik = hmm.viterbi("AA", E)
+    assert_allclose(lik, LOG(0.32))
 
-# def test_hmm_delete_state():
-#     hmm = _create_hmm()
+    lik = hmm.viterbi("CA", E)
+    assert_allclose(lik, LOG(0.08))
 
-#     with pytest.raises(ValueError):
-#         hmm.delete_state("D22")
+    lik = hmm.viterbi("CC", E)
+    assert_allclose(lik, LOG(0.12))
 
-#     hmm.delete_state("D2")
-#     assert "D2" not in hmm.states
+    hmm.set_trans(M1, E, LOG(1.0))
 
+    lik = hmm.viterbi("AC", E)
+    assert_allclose(lik, LOG(0.48))
 
-# def test_hmm_single_state():
-#     alphabet = "ACGU"
-#     hmm = HMM(alphabet)
-#     state = NormalState(
-#         "I", {"A": LOG(0.8), "C": LOG(0.2), "G": LOG(0.0), "U": LOG(0.0)}
-#     )
-#     hmm.add_state(state)
-#     hmm.set_trans("I", "I", LOG(1.0))
-#     hmm.normalize()
-#     lik, path = hmm.viterbi("ACC", "I")
-#     assert abs(lik - 0.032) < 1e-7
-#     assert path == [("I", 1), ("I", 1), ("I", 1)]
+    lik = hmm.viterbi("AA", E)
+    assert_allclose(lik, LOG(0.32))
 
 
-# def _create_hmm():
-#     alphabet = "AC"
+def test_hmm_viterbi_3():
+    alphabet = Alphabet("AC")
 
-#     hmm = HMM(alphabet)
-#     start_state = MuteState("S", alphabet)
-#     hmm.add_state(start_state, LOG(1.0))
+    hmm = HMM(alphabet)
+    S = MuteState("S", alphabet)
+    hmm.add_state(S, LOG(1.0))
 
-#     end_state = MuteState("E", alphabet)
-#     hmm.add_state(end_state, LOG(0.0))
+    E = MuteState("E", alphabet)
+    hmm.add_state(E, LOG(0.0))
 
-#     M1 = NormalState("M1", {"A": LOG(0.8), "C": LOG(0.2)})
-#     hmm.add_state(M1, LOG(0.0))
+    M1 = NormalState("M1", alphabet, [LOG(0.8), LOG(0.2)])
+    hmm.add_state(M1, LOG(0.0))
 
-#     D1 = MuteState("D1", alphabet)
-#     hmm.add_state(D1, LOG(0.0))
+    D1 = MuteState("D1", alphabet)
+    hmm.add_state(D1, LOG(0.0))
 
-#     M2 = NormalState("M2", {"A": LOG(0.4), "C": LOG(0.6)})
-#     hmm.add_state(M2, LOG(0.0))
+    M2 = NormalState("M2", alphabet, [LOG(0.4), LOG(0.6)])
+    hmm.add_state(M2, LOG(0.0))
 
-#     D2 = MuteState("D2", alphabet)
-#     hmm.add_state(D2, LOG(0.0))
+    D2 = MuteState("D2", alphabet)
+    hmm.add_state(D2, LOG(0.0))
 
-#     hmm.set_trans("S", "M1", LOG(0.8))
-#     hmm.set_trans("S", "D1", LOG(0.2))
+    hmm.set_trans(S, M1, LOG(0.8))
+    hmm.set_trans(S, D1, LOG(0.2))
 
-#     hmm.set_trans("M1", "M2", LOG(0.8))
-#     hmm.set_trans("M1", "D2", LOG(0.2))
+    hmm.set_trans(M1, M2, LOG(0.8))
+    hmm.set_trans(M1, D2, LOG(0.2))
 
-#     hmm.set_trans("D1", "D2", LOG(0.2))
-#     hmm.set_trans("D1", "M2", LOG(0.8182787382))
+    hmm.set_trans(D1, D2, LOG(0.2))
+    hmm.set_trans(D1, M2, LOG(0.8))
 
-#     hmm.set_trans("D2", "E", LOG(1.0))
-#     hmm.set_trans("M2", "E", LOG(1.0))
+    hmm.set_trans(D2, E, LOG(1.0))
+    hmm.set_trans(M2, E, LOG(1.0))
+    hmm.set_trans(E, E, LOG(1.0))
+    hmm.normalize()
+    hmm.set_trans(E, E, LOG(0.0))
 
-#     return hmm
+    lik = hmm.viterbi("AC", E)
+    assert_allclose(lik, LOG(0.3072))
+
+    lik = hmm.viterbi("AA", E)
+    assert_allclose(lik, LOG(0.2048))
+
+    lik = hmm.viterbi("A", E)
+    assert_allclose(lik, LOG(0.128))
+
+    lik = hmm.viterbi("AC", E)
+    assert_allclose(lik, LOG(0.3072))
+
+    lik = hmm.viterbi("AC", M2)
+    assert_allclose(lik, LOG(0.3072))
+
+    hmm.del_state(E)
+
+    lik = hmm.viterbi("AC", M2)
+    assert_allclose(lik, LOG(0.3072))
