a45b8eabc83ebc337d34cb2decdf7c88a359d43e
Danilo Horta <danilo.horta@pm.me>
2019-11-06 13:10:05 +0000

    CStep

--- nmm/_step.py
+++ nmm/_step.py
@@ -1,12 +1,32 @@
-from ._state import State
+from ._state import State, CState
 
-# from ._ffi import ffi, lib
+from ._ffi import ffi, lib
 
 
-class Step:
+class CStep:
+    def __init__(self, cdata: ffi.CData):
+        self.__cdata = cdata
+
+    @property
+    def imm_step(self) -> ffi.CData:
+        if self.__cdata == ffi.NULL:
+            raise RuntimeError("`imm_step` is NULL.")
+        return self.__cdata
+
+    @property
+    def state(self) -> CState:
+        return CState(lib.imm_step_state(self.imm_step))
+
+    @property
+    def seq_len(self) -> int:
+        return lib.imm_step_seq_len(self.imm_step)
+
+
+class Step(CStep):
     def __init__(self, state: State, seq_len: int):
         self._state = state
         self._seq_len = seq_len
+        super().__init__(ffi.NULL)
 
     @property
     def state(self) -> State:
