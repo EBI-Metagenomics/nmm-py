09058691ac31660307840adaf87907f9ccbdfeb4
Danilo Horta <danilo.horta@pm.me>
2019-11-26 12:39:45 +0000

    tests on decoded codons, prepare for refactoring

--- nmm/test/test_hmmer_frame.py
+++ nmm/test/test_hmmer_frame.py
@@ -94,21 +94,36 @@ def test_read_hmmer_frame_6(tmp_path):
     rna_seq = b"AAGA AAA AAA CCU GGU AAA GAA GAU AAU AAC AAA"
     rna_seq = rna_seq.replace(b" ", b"")
 
-    r = hmmer.lr(rna_seq)[0]
+    r, cr = hmmer.lr(rna_seq)
     assert_allclose(r.score, 171.5602273844319)
     frags = r.fragments
+    cfrags = cr.fragments
 
     assert_equal(len(frags), 2)
+    assert_equal(len(cfrags), 2)
     assert_equal(frags[0].homologous, False)
+    assert_equal(cfrags[0].homologous, False)
     assert_equal(frags[0].sequence, b"AAGAAAAAAA")
+    assert_equal(cfrags[0].sequence, b"AAGAAAAAA")
 
     items = list(frags[0].items())
+    citems = list(cfrags[0].items())
     assert_equal(items[0][0], b"")
     assert_equal(str(items[0][1]), "<S,0>")
+    assert_equal(citems[0][0], b"")
+    assert_equal(str(citems[0][1]), "<S,0>")
+
     assert_equal(items[1][0], b"AAG")
     assert_equal(str(items[1][1]), "<N,3>")
+    assert_equal(citems[1][0], b"AAG")
+    assert_equal(str(citems[1][1]), "<N,3>")
+
     assert_equal(items[2][0], b"AAAA")
     assert_equal(str(items[2][1]), "<N,4>")
+
+    assert_equal(citems[2][0], b"AAA")
+    assert_equal(str(citems[2][1]), "<N,3>")
+
     assert_equal(items[3][0], b"AAA")
     assert_equal(str(items[3][1]), "<N,3>")
     assert_equal(items[4][0], b"")
@@ -122,6 +137,8 @@ def test_read_hmmer_frame_6(tmp_path):
     assert_equal(str(items[0][1]), "<M1,3>")
     assert_equal(items[7][0], b"AAA")
     assert_equal(str(items[7][1]), "<M8,3>")
+
+    pass
 
 
 def write_file(path, filename):
