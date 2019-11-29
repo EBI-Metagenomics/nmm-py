d319ebe48b32a17c3e0b8e8b4abc2ed3c98e7143
Danilo Horta <danilo.horta@pm.me>
2019-11-29 16:37:16 +0000

    more tests on decode

--- nmm/_hmmer/test/test_frame_profile.py
+++ nmm/_hmmer/test/test_frame_profile.py
@@ -120,16 +120,17 @@ def test_frame_profile_codons(PF03373):
     hmmer = create_frame_profile(reader, epsilon=0.1)
 
     # seq = b"KKKPGKEDNNK"
-    rna_seq = b"AAGA AAA AAA CCU GGU AAA GAA GAU AAU AAC AAA"
+    rna_seq = b"AAGA AAA AAA CCU GGU AAA GAA GAU AAU AAC AAA G"
     rna_seq = rna_seq.replace(b" ", b"")
 
     r = hmmer.search(rna_seq)
-    assert_allclose(r.score, 171.5602273844319)
+    assert_allclose(r.score, 175.35113397356454)
     frags = r.fragments
     cfrags = [f.convert_to_codons() for f in frags]
 
     assert_equal(len(frags), 2)
     assert_equal(len(cfrags), 2)
+
     assert_equal(frags[0].homologous, False)
     assert_equal(cfrags[0].homologous, False)
     assert_equal(frags[0].sequence, b"AAGAAAAAAA")
@@ -137,6 +138,7 @@ def test_frame_profile_codons(PF03373):
 
     items = list(frags[0].items())
     citems = list(cfrags[0].items())
+
     assert_equal(items[0][0], b"")
     assert_equal(str(items[0][1]), "<S,0>")
     assert_equal(citems[0][0], b"")
@@ -149,20 +151,33 @@ def test_frame_profile_codons(PF03373):
 
     assert_equal(items[2][0], b"AAAA")
     assert_equal(str(items[2][1]), "<N,4>")
-
     assert_equal(citems[2][0], b"AAA")
     assert_equal(str(citems[2][1]), "<N,3>")
 
     assert_equal(items[3][0], b"AAA")
     assert_equal(str(items[3][1]), "<N,3>")
+    assert_equal(citems[3][0], b"AAA")
+    assert_equal(str(citems[3][1]), "<N,3>")
+
     assert_equal(items[4][0], b"")
     assert_equal(str(items[4][1]), "<B,0>")
+    assert_equal(citems[4][0], b"")
+    assert_equal(str(citems[4][1]), "<B,0>")
 
     assert_equal(frags[1].homologous, True)
-    assert_equal(frags[1].sequence, b"CCUGGUAAAGAAGAUAAUAACAAA")
+    assert_equal(cfrags[1].homologous, True)
+    assert_equal(frags[1].sequence, b"CCUGGUAAAGAAGAUAAUAACAAAG")
+    assert_equal(cfrags[1].sequence, b"CCUGGUAAAGAAGAUAAUAACAAG")
 
     items = list(frags[1].items())
+    citems = list(cfrags[1].items())
+
     assert_equal(items[0][0], b"CCU")
     assert_equal(str(items[0][1]), "<M1,3>")
-    assert_equal(items[7][0], b"AAA")
-    assert_equal(str(items[7][1]), "<M8,3>")
+    assert_equal(citems[0][0], b"CCU")
+    assert_equal(str(citems[0][1]), "<M1,3>")
+
+    assert_equal(items[7][0], b"AAAG")
+    assert_equal(str(items[7][1]), "<M8,4>")
+    assert_equal(citems[7][0], b"AAG")
+    assert_equal(str(citems[7][1]), "<M8,3>")
