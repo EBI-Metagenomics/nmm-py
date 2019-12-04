e85143728318bc0ba907471e1d9b0668da33bd63
Danilo Horta <danilo.horta@pm.me>
2019-12-04 19:40:33 +0000

    cmake-3.9.6

--- ci/prepare-linux.sh
+++ ci/prepare-linux.sh
@@ -9,8 +9,8 @@ else
     TAG='i386';
 fi
 
-curl -O https://cmake.org/files/v3.8/cmake-3.8.2-Linux-$TAG.sh
-bash cmake-3.8.2-Linux-$TAG.sh --skip-license --prefix=/
+curl -O https://cmake.org/files/v3.9/cmake-3.9.6-Linux-$TAG.sh
+bash cmake-3.9.6-Linux-$TAG.sh --skip-license --prefix=/
 
 hash -r
 which cmake
