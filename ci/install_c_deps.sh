2f52cf409eee3e1558c9e3c632bfdc8edd784a3f
Danilo Horta <danilo.horta@pm.me>
2019-12-04 16:12:53 +0000

    export DO_CMD=sudo if not windows

--- ci/install_c_deps.sh
+++ ci/install_c_deps.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+if [ "$TRAVIS_OS_NAME" != "windows"]; then
+    export DO_CMD=sudo
+fi
+
 python -m pip install numpy
-curl -fsSL https://git.io/JerYI | GITHUB_USER=horta GITHUB_PROJECT=logaddexp DO_CMD=sudo bash
-curl -fsSL https://git.io/JerYI | GITHUB_USER=EBI-Metagenomics GITHUB_PROJECT=imm DO_CMD=sudo bash
-curl -fsSL https://git.io/JerYI | GITHUB_USER=EBI-Metagenomics GITHUB_PROJECT=nmm DO_CMD=sudo bash
+curl -fsSL https://git.io/JerYI | GITHUB_USER=horta GITHUB_PROJECT=logaddexp bash
+curl -fsSL https://git.io/JerYI | GITHUB_USER=EBI-Metagenomics GITHUB_PROJECT=imm bash
+curl -fsSL https://git.io/JerYI | GITHUB_USER=EBI-Metagenomics GITHUB_PROJECT=nmm bash
