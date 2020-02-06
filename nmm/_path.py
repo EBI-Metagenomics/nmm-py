dbc5c53e339594a9e9e35a031565fac83a41566c
Danilo Horta <danilo.horta@pm.me>
2020-02-06 18:35:12 +0000

    path, step

--- nmm/_path.py
+++ nmm/_path.py
@@ -1,42 +1,40 @@
-from abc import ABC, abstractmethod
-from typing import Iterator, Optional, Sequence, Tuple, Type, TypeVar
+from typing import Iterator
 
 from ._ffi import ffi, lib
-from ._state import CState
-from ._step import CStep, StepBase
+
+# from ._state import CState
+from ._step import CStep
 
 
-class PathBase(ABC):
-    @abstractmethod
-    def steps(self) -> Iterator[StepBase]:
-        raise NotImplementedError()
+# class PathBase(ABC):
+#     @abstractmethod
+#     def steps(self) -> Iterator[StepBase]:
+#         raise NotImplementedError()
 
 
-T = TypeVar("T", bound="CPath")
+# T = TypeVar("T", bound="CPath")
 
 
-class CPath(PathBase):
+class CPath:
     """
     Wrapper around the C implementation of path.
 
     Parameters
     ----------
-    imm_path : Optional[ffi.CData]
-        `<cdata 'struct imm_path *'>` or `None`. Passing `None` will create a new path at the
-        underlying library level.
+    imm_path : `<cdata 'struct imm_path *'>`
     """
 
-    def __init__(self, imm_path: Optional[ffi.CData] = None):
-        if imm_path is None:
-            imm_path = _create_imm_path()
+    def __init__(self, imm_path: ffi.CData):
+        if imm_path == ffi.NULL:
+            raise RuntimeError("`imm_path` is NULL.")
         self._imm_path = imm_path
 
-    @classmethod
-    def create_cpath(cls: Type[T], steps: Sequence[Tuple[CState, int]] = []) -> T:
-        cpath = cls()
-        for step in steps:
-            cpath.append_cstep(step[0], step[1])
-        return cpath
+    # @classmethod
+    # def create_cpath(cls: Type[T], steps: Sequence[Tuple[CState, int]] = []) -> T:
+    #     cpath = cls()
+    #     for step in steps:
+    #         cpath.append_cstep(step[0], step[1])
+    #     return cpath
 
     @property
     def imm_path(self) -> ffi.CData:
@@ -48,27 +46,28 @@ class CPath(PathBase):
             yield CStep(step)
             step = lib.imm_path_next(self._imm_path, step)
 
-    def append_cstep(self, state: CState, seq_len: int) -> CStep:
+    # def append_cstep(self, state: CState, seq_len: int) -> CStep:
 
-        err: int = lib.imm_path_append(self._imm_path, state.imm_state, seq_len)
-        if err != 0:
-            raise RuntimeError("Could not add step.")
+    #     err: int = lib.imm_path_append(self._imm_path, state.imm_state, seq_len)
+    #     if err != 0:
+    #         raise RuntimeError("Could not add step.")
 
-        imm_step = lib.imm_path_last(self._imm_path)
-        return CStep(imm_step)
+    #     imm_step = lib.imm_path_last(self._imm_path)
+    #     return CStep(imm_step)
 
     def __del__(self):
         if self._imm_path != ffi.NULL:
             lib.imm_path_destroy(self._imm_path)
 
+    def __str__(self) -> str:
+        return ",".join([str(s) for s in self.steps()])
+
     def __repr__(self):
-        step_repr = [f"<{s.state.name.decode()}:{s.seq_len}>" for s in self.steps()]
-        msg = "".join(step_repr)
-        return f"<{self.__class__.__name__}:{msg}>"
+        return f"<{self.__class__.__name__}:{str(self)}>"
 
 
-def _create_imm_path():
-    imm_path = lib.imm_path_create()
-    if imm_path == ffi.NULL:
-        raise RuntimeError("`imm_path_create` failed.")
-    return imm_path
+# def _create_imm_path():
+# imm_path = lib.imm_path_create()
+# if imm_path == ffi.NULL:
+#     raise RuntimeError("`imm_path_create` failed.")
+# return imm_path
--- nmm/_step.py
+++ nmm/_step.py
@@ -1,48 +1,19 @@
-from abc import ABC, abstractmethod
-
 from ._ffi import ffi, lib
-from ._state import CState, StateBase
 
 
-class StepBase(ABC):
+class CStep:
     """
-    Path step.
+    Wrapper around the C implementation of a path step.
 
     A step is composed of a state and an emitted sequence length. The user should not need to
     directly call the constructor of this class but instead use the methods from the `Path` class.
-    """
-
-    @property
-    @abstractmethod
-    def state(self) -> StateBase:
-        raise NotImplementedError()
-
-    @property
-    @abstractmethod
-    def seq_len(self) -> int:
-        raise NotImplementedError()
-
-    def __str__(self) -> str:
-        name = self.state.name.decode()
-        return f"<{name},{self.seq_len}>"
-
-    def __repr__(self) -> str:
-        name = self.state.name.decode()
-        return f"<{self.__class__.__name__}:{name},{self.seq_len}>"
-
-
-class CStep(StepBase):
-    """
-    Wrapper around the C implementation of path step.
 
     Parameters
     ----------
-    imm_step : ffi.CData
-        A non-null `<cdata 'struct imm_step *'>`.
+    imm_step : `<cdata 'struct imm_step *'>`
     """
 
     def __init__(self, imm_step: ffi.CData):
-        super().__init__()
         if imm_step == ffi.NULL:
             raise RuntimeError("`imm_step` is NULL.")
         self._imm_step = imm_step
@@ -52,13 +23,13 @@ class CStep(StepBase):
         return self._imm_step
 
     @property
-    def state(self) -> CState:
-        return CState(lib.imm_step_state(self.imm_step))
-
-    @property
     def seq_len(self) -> int:
         return lib.imm_step_seq_len(self.imm_step)
 
+    def __str__(self) -> str:
+        state = lib.imm_step_state(self._imm_step)
+        name: str = ffi.string(lib.imm_state_get_name(state))
+        return f"{name},{self.seq_len}"
+
     def __repr__(self) -> str:
-        name = self.state.name.decode()
-        return f"<{self.__class__.__name__}:{name},{self.seq_len}>"
+        return f"<{self.__class__.__name__}:{str(self)}>"
