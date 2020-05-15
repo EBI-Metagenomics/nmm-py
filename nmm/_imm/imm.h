c3a0dcc10263a050d7c5930382f8592a3da584c0
Danilo Horta <danilo.horta@pm.me>
2020-05-15 03:04:06 +0100

    update C interface

--- nmm/_imm/imm.h
+++ nmm/_imm/imm.h
@@ -1,8 +1,12 @@
 struct imm_abc;
+struct imm_abc_table;
 struct imm_dp;
 struct imm_hmm;
+struct imm_input;
+struct imm_model;
 struct imm_mute_state;
 struct imm_normal_state;
+struct imm_output;
 struct imm_path;
 struct imm_result;
 struct imm_results;
@@ -10,6 +14,7 @@ struct imm_seq_table;
 struct imm_state;
 struct imm_step;
 struct imm_table_state;
+struct imm_window;
 
 enum imm_symbol_type
 {
@@ -32,122 +37,170 @@ struct imm_subseq
 };
 
 /* Alphabet */
-struct imm_abc const *imm_abc_create(char const *symbols, char any_symbol);
-struct imm_abc const *imm_abc_clone(struct imm_abc const *abc);
-void                  imm_abc_destroy(struct imm_abc const *abc);
-unsigned              imm_abc_length(struct imm_abc const *abc);
-char const *          imm_abc_symbols(struct imm_abc const *abc);
-bool                  imm_abc_has_symbol(struct imm_abc const *abc, char symbol_id);
-int                   imm_abc_symbol_idx(struct imm_abc const *abc, char symbol_id);
-char                  imm_abc_symbol_id(struct imm_abc const *abc, unsigned symbol_idx);
 char                  imm_abc_any_symbol(struct imm_abc const *abc);
+struct imm_abc const *imm_abc_clone(struct imm_abc const *abc);
+struct imm_abc const *imm_abc_create(char const *symbols, char any_symbol);
+void                  imm_abc_destroy(struct imm_abc const *abc);
+bool                  imm_abc_has_symbol(struct imm_abc const *abc, char symbol_id);
+uint8_t               imm_abc_length(struct imm_abc const *abc);
+struct imm_abc *      imm_abc_read(FILE *stream);
+char                  imm_abc_symbol_id(struct imm_abc const *abc, uint8_t symbol_idx);
+uint8_t               imm_abc_symbol_idx(struct imm_abc const *abc, char symbol_id);
 enum imm_symbol_type  imm_abc_symbol_type(struct imm_abc const *abc, char symbol_id);
+char const *          imm_abc_symbols(struct imm_abc const *abc);
+uint8_t               imm_abc_type_id(struct imm_abc const *abc);
+int                   imm_abc_write(struct imm_abc const *abc, FILE *stream);
 
 /* Alphabet table */
 struct imm_abc_table const *imm_abc_table_create(struct imm_abc const *abc, double const *lprobs);
-double                      imm_abc_table_lprob(struct imm_abc_table const *abc_table, char symbol);
 void                        imm_abc_table_destroy(struct imm_abc_table const *abc_table);
 struct imm_abc const *      imm_abc_table_get_abc(struct imm_abc_table const *abc_table);
+double                      imm_abc_table_lprob(struct imm_abc_table const *abc_table, char symbol);
 
-/* Sequence */
-struct imm_seq const *imm_seq_create(char const *seq, struct imm_abc const *abc);
-struct imm_abc const *imm_seq_get_abc(struct imm_seq const *seq);
-unsigned              imm_seq_length(struct imm_seq const *seq);
-char const *          imm_seq_string(struct imm_seq const *seq);
-struct imm_seq const *imm_seq_clone(struct imm_seq const *seq);
-void                  imm_seq_destroy(struct imm_seq const *seq);
+/* DP */
+void                      imm_dp_destroy(struct imm_dp const *dp);
+struct imm_results const *imm_dp_viterbi(struct imm_dp const *dp, struct imm_seq const *seq,
+                                         unsigned window_length);
 
-/* Subsequence */
-struct imm_subseq     imm_subseq_slice(struct imm_seq const *seq, unsigned start, unsigned length);
-struct imm_seq const *imm_subseq_cast(struct imm_subseq const *subseq);
-void                  imm_subseq_set(struct imm_subseq *subseq, unsigned start, unsigned length);
-unsigned              imm_subseq_start(struct imm_subseq const *subseq);
-unsigned              imm_subseq_length(struct imm_subseq const *subseq);
+/* HMM */
+int imm_hmm_add_state(struct imm_hmm *hmm, struct imm_state const *state, double start_lprob);
+struct imm_hmm *     imm_hmm_create(struct imm_abc const *abc);
+struct imm_dp const *imm_hmm_create_dp(struct imm_hmm const *hmm, struct imm_state const *end_state);
+int                  imm_hmm_del_state(struct imm_hmm *hmm, struct imm_state const *state);
+void                 imm_hmm_destroy(struct imm_hmm const *hmm);
+double               imm_hmm_get_trans(struct imm_hmm const *hmm, struct imm_state const *src_state,
+                                       struct imm_state const *tgt_state);
+double               imm_hmm_likelihood(struct imm_hmm const *hmm, struct imm_seq const *seq,
+                                        struct imm_path const *path);
+int                  imm_hmm_normalize(struct imm_hmm *hmm);
+int                  imm_hmm_normalize_start(struct imm_hmm *hmm);
+int                  imm_hmm_normalize_trans(struct imm_hmm *hmm, struct imm_state const *src_state);
+int imm_hmm_set_start(struct imm_hmm *hmm, struct imm_state const *state, double lprob);
+int imm_hmm_set_trans(struct imm_hmm *hmm, struct imm_state const *src_state,
+                      struct imm_state const *tgt_state, double lprob);
 
-/* Sequence table */
-struct imm_seq_table *imm_seq_table_create(struct imm_abc const *abc);
-struct imm_seq_table *imm_seq_table_clone(struct imm_seq_table const *table);
-void                  imm_seq_table_destroy(struct imm_seq_table const *table);
-int    imm_seq_table_add(struct imm_seq_table *table, struct imm_seq const *seq, double lprob);
-int    imm_seq_table_normalize(struct imm_seq_table *table);
-double imm_seq_table_lprob(struct imm_seq_table const *table, struct imm_seq const *seq);
-struct imm_abc const *imm_seq_table_get_abc(struct imm_seq_table const *table);
-unsigned              imm_seq_table_min_seq(struct imm_seq_table const *table);
-unsigned              imm_seq_table_max_seq(struct imm_seq_table const *table);
+/* Input */
+struct imm_input *      imm_input_create(char const *filepath);
+int                     imm_input_destroy(struct imm_input const *input);
+bool                    imm_input_eof(struct imm_input const *input);
+struct imm_model const *imm_input_read(struct imm_input *input);
 
-/* State */
-char const *            imm_state_get_name(struct imm_state const *state);
-struct imm_abc const *  imm_state_get_abc(struct imm_state const *state);
-double                  imm_state_lprob(struct imm_state const *state, struct imm_seq const *seq);
-unsigned                imm_state_min_seq(struct imm_state const *state);
-unsigned                imm_state_max_seq(struct imm_state const *state);
-struct imm_state const *imm_state_cast_c(void const *state);
-void const *            imm_state_get_impl_c(struct imm_state const *state);
+/* Model */
+struct imm_abc const *  imm_model_abc(struct imm_model const *model);
+struct imm_model *      imm_model_create(struct imm_hmm *hmm, struct imm_dp const *dp);
+void                    imm_model_destroy(struct imm_model const *model);
+struct imm_dp const *   imm_model_dp(struct imm_model const *model);
+struct imm_hmm *        imm_model_hmm(struct imm_model const *model);
+uint32_t                imm_model_nstates(struct imm_model const *model);
+struct imm_model const *imm_model_read(FILE *stream);
+struct imm_state const *imm_model_state(struct imm_model const *model, uint32_t i);
+int                     imm_model_write(struct imm_model const *model, FILE *stream);
+
+/* Mute state */
+struct imm_mute_state const *imm_mute_state_create(char const *name, struct imm_abc const *abc);
+struct imm_mute_state const *imm_mute_state_derived(struct imm_state const *state);
+void                         imm_mute_state_destroy(struct imm_mute_state const *state);
+struct imm_state const *     imm_mute_state_read(FILE *stream, struct imm_abc const *abc);
+struct imm_state const *     imm_mute_state_super(struct imm_mute_state const *state);
+int imm_mute_state_write(struct imm_state const *state, struct imm_model const *model, FILE *stream);
 
 /* Normal state */
 struct imm_normal_state const *imm_normal_state_create(char const *name, struct imm_abc const *abc,
                                                        double const *lprobs);
+struct imm_normal_state const *imm_normal_state_derived(struct imm_state const *state);
 void                           imm_normal_state_destroy(struct imm_normal_state const *state);
+struct imm_state const *       imm_normal_state_read(FILE *stream, struct imm_abc const *abc);
+struct imm_state const *       imm_normal_state_super(struct imm_normal_state const *state);
+int imm_normal_state_write(struct imm_state const *state, struct imm_model const *model,
+                           FILE *stream);
 
-/* Mute state */
-struct imm_mute_state const *imm_mute_state_create(char const *name, struct imm_abc const *abc);
-void                         imm_mute_state_destroy(struct imm_mute_state const *state);
+/* Output */
+struct imm_output *imm_output_create(char const *filepath);
+int                imm_output_destroy(struct imm_output const *output);
+int                imm_output_write(struct imm_output *output, struct imm_model const *model);
 
-/* Table state */
-struct imm_table_state *imm_table_state_create(char const *name, struct imm_seq_table const *table);
-void                    imm_table_state_destroy(struct imm_table_state const *state);
-
-/* Results */
-struct imm_results *imm_results_create(struct imm_seq const *seq, unsigned nresults);
-void imm_results_set(struct imm_results *results, unsigned idx, struct imm_subseq subseq,
-                     struct imm_path const *path, double loglik);
-struct imm_result const *imm_results_get(struct imm_results const *results, unsigned idx);
-unsigned                 imm_results_size(struct imm_results const *results);
-void                     imm_results_destroy(struct imm_results const *results);
-void                     imm_results_free(struct imm_results const *results);
+/* Path */
+void                   imm_path_append(struct imm_path *path, struct imm_step *step);
+struct imm_path *      imm_path_clone(struct imm_path const *path);
+struct imm_path *      imm_path_create(void);
+void                   imm_path_destroy(struct imm_path const *path);
+struct imm_step const *imm_path_first(struct imm_path const *path);
+void                   imm_path_free(struct imm_path const *path);
+struct imm_step const *imm_path_next(struct imm_path const *path, struct imm_step const *step);
+void                   imm_path_prepend(struct imm_path *path, struct imm_step *step);
 
 /* Result */
+void                   imm_result_destroy(struct imm_result const *result);
 double                 imm_result_loglik(struct imm_result const *result);
 struct imm_path const *imm_result_path(struct imm_result const *result);
 struct imm_subseq      imm_result_subseq(struct imm_result const *result);
-void                   imm_result_destroy(struct imm_result const *result);
-void                   imm_result_free(struct imm_result const *result);
 
-/* HMM */
-struct imm_hmm *imm_hmm_create(struct imm_abc const *abc);
-void            imm_hmm_destroy(struct imm_hmm *hmm);
-int    imm_hmm_add_state(struct imm_hmm *hmm, struct imm_state const *state, double start_lprob);
-int    imm_hmm_del_state(struct imm_hmm *hmm, struct imm_state const *state);
-int    imm_hmm_set_start(struct imm_hmm *hmm, struct imm_state const *state, double lprob);
-int    imm_hmm_set_trans(struct imm_hmm *hmm, struct imm_state const *src_state,
-                         struct imm_state const *dst_state, double lprob);
-double imm_hmm_get_trans(struct imm_hmm const *hmm, struct imm_state const *src_state,
-                         struct imm_state const *dst_state);
-double imm_hmm_likelihood(struct imm_hmm const *hmm, struct imm_seq const *seq,
-                          struct imm_path const *path);
-struct imm_dp const *imm_hmm_create_dp(struct imm_hmm const *hmm, struct imm_state const *end_state);
-int                  imm_hmm_normalize(struct imm_hmm *hmm);
-int                  imm_hmm_normalize_start(struct imm_hmm *hmm);
-int                  imm_hmm_normalize_trans(struct imm_hmm *hmm, struct imm_state const *src);
+/* Results */
+struct imm_results *     imm_results_create(struct imm_seq const *seq, unsigned nresults);
+void                     imm_results_destroy(struct imm_results const *results);
+struct imm_result const *imm_results_get(struct imm_results const *results, unsigned idx);
+void     imm_results_set(struct imm_results *results, unsigned idx, struct imm_subseq subseq,
+                         struct imm_path const *path, double loglik);
+unsigned imm_results_size(struct imm_results const *results);
 
-/* DP */
-struct imm_results const *imm_dp_viterbi(struct imm_dp const *dp, struct imm_seq const *seq,
-                                         unsigned window_length);
-void                      imm_dp_destroy(struct imm_dp const *dp);
+/* Sequence */
+struct imm_seq        IMM_SEQ(struct imm_abc const *abc, char const *string, unsigned length);
+struct imm_seq const *imm_seq_clone(struct imm_seq const *seq);
+struct imm_seq const *imm_seq_create(char const *string, struct imm_abc const *abc);
+void                  imm_seq_destroy(struct imm_seq const *seq);
+struct imm_abc const *imm_seq_get_abc(struct imm_seq const *seq);
+unsigned              imm_seq_length(struct imm_seq const *seq);
+char const *          imm_seq_string(struct imm_seq const *seq);
 
-/* Path */
-struct imm_path *      imm_path_create(void);
-struct imm_path *      imm_path_clone(struct imm_path const *path);
-void                   imm_path_free(struct imm_path const *path);
-void                   imm_path_destroy(struct imm_path const *path);
-void                   imm_path_append(struct imm_path *path, struct imm_step *step);
-void                   imm_path_prepend(struct imm_path *path, struct imm_step *step);
-struct imm_step const *imm_path_first(struct imm_path const *path);
-struct imm_step const *imm_path_next(struct imm_path const *path, struct imm_step const *step);
+/* Sequence table */
+struct imm_abc const *imm_seq_table_abc(struct imm_seq_table const *table);
+int imm_seq_table_add(struct imm_seq_table *table, struct imm_seq const *seq, double lprob);
+struct imm_seq_table *imm_seq_table_clone(struct imm_seq_table const *table);
+struct imm_seq_table *imm_seq_table_create(struct imm_abc const *abc);
+void                  imm_seq_table_destroy(struct imm_seq_table const *table);
+double  imm_seq_table_lprob(struct imm_seq_table const *table, struct imm_seq const *seq);
+uint8_t imm_seq_table_max_seq(struct imm_seq_table const *table);
+uint8_t imm_seq_table_min_seq(struct imm_seq_table const *table);
+int     imm_seq_table_normalize(struct imm_seq_table *table);
+
+/* State */
+struct imm_state const *imm_state_create(char const *name, struct imm_abc const *abc,
+                                         struct imm_state_vtable vtable, void *derived);
+void                    imm_state_destroy(struct imm_state const *state);
+struct imm_abc const *  imm_state_get_abc(struct imm_state const *state);
+char const *            imm_state_get_name(struct imm_state const *state) { return state->name; }
+double                  imm_state_lprob(struct imm_state const *state, struct imm_seq const *seq);
+uint8_t                 imm_state_max_seq(struct imm_state const *state);
+uint8_t                 imm_state_min_seq(struct imm_state const *state);
+uint8_t                 imm_state_type_id(struct imm_state const *state);
 
 /* Step */
-struct imm_step *       imm_step_create(struct imm_state const *state, unsigned seq_len);
-struct imm_state const *imm_step_state(struct imm_step const *step);
-unsigned                imm_step_seq_len(struct imm_step const *step);
-void                    imm_step_destroy(struct imm_step const *step);
 struct imm_step *       imm_step_clone(struct imm_step const *step);
+struct imm_step *       imm_step_create(struct imm_state const *state, unsigned seq_len);
+void                    imm_step_destroy(struct imm_step const *step);
+unsigned                imm_step_seq_len(struct imm_step const *step);
+struct imm_state const *imm_step_state(struct imm_step const *step);
+
+/* Subsequence */
+struct imm_seq const *imm_subseq_cast(struct imm_subseq const *subseq);
+struct imm_subseq     imm_subseq_init(struct imm_subseq *subseq, struct imm_seq const *seq,
+                                      unsigned start, unsigned length);
+unsigned              imm_subseq_length(struct imm_subseq const *subseq);
+void                  imm_subseq_set(struct imm_subseq *subseq, unsigned start, unsigned length);
+struct imm_subseq     imm_subseq_slice(struct imm_seq const *seq, unsigned start, unsigned length);
+unsigned              imm_subseq_start(struct imm_subseq const *subseq);
+
+/* Table state */
+struct imm_table_state const *imm_table_state_create(char const *                name,
+                                                     struct imm_seq_table const *table);
+struct imm_table_state const *imm_table_state_derived(struct imm_state const *state);
+void                          imm_table_state_destroy(struct imm_table_state const *state);
+struct imm_state const *      imm_table_state_read(FILE *stream, struct imm_abc const *abc);
+struct imm_state const *      imm_table_state_super(struct imm_table_state const *state);
+int imm_table_state_write(struct imm_state const *state, struct imm_model const *model, FILE *stream);
+
+/* Window */
+struct imm_window *imm_window_create(struct imm_seq const *seq, unsigned length);
+void               imm_window_destroy(struct imm_window const *window);
+struct imm_subseq  imm_window_get(struct imm_window const *window, unsigned index);
+unsigned           imm_window_size(struct imm_window const *window);
--- nmm/_nmm/nmm.h
+++ nmm/_nmm/nmm.h
@@ -1,9 +1,16 @@
+struct imm_state;
+struct nmm_amino_abc;
+struct nmm_amino_table;
 struct nmm_base_abc;
-struct nmm_codon;
 struct nmm_base_table;
+struct nmm_codon;
 struct nmm_codon_lprob;
+struct nmm_codon_state;
 struct nmm_codon_table;
 struct nmm_frame_state;
+struct nmm_input;
+struct nmm_model;
+struct nmm_output;
 
 struct nmm_triplet
 {
@@ -13,57 +20,104 @@ struct nmm_triplet
 };
 
 /* Amino abc */
-struct nmm_amino_abc const *nmm_amino_abc_create(struct imm_abc const *abc);
+struct nmm_amino_abc const *nmm_amino_abc_create(char const *symbols, char const any_symbol);
+struct nmm_amino_abc const *nmm_amino_abc_derived(struct imm_abc const *abc);
 void                        nmm_amino_abc_destroy(struct nmm_amino_abc const *amino_abc);
-struct imm_abc const *      nmm_amino_abc_cast(struct nmm_amino_abc const *amino_abc);
+struct imm_abc const *      nmm_amino_abc_super(struct nmm_amino_abc const *amino_abc);
 
 /* Amino table */
-struct nmm_amino_table const *nmm_amino_table_create(struct nmm_amino_abc const *amino_abc,
+struct nmm_amino_table const *nmm_amino_table_create(struct nmm_amino_abc const *abc,
                                                      double const *              lprobs);
-double                        nmm_amino_table_lprob(struct nmm_amino_table const *aminot, char amino);
-void                          nmm_amino_table_destroy(struct nmm_amino_table const *aminot);
-struct nmm_amino_abc const *  nmm_amino_table_get_amino_abc(struct nmm_amino_table const *aminot);
+void                          nmm_amino_table_destroy(struct nmm_amino_table const *tbl);
+struct nmm_amino_abc const *  nmm_amino_table_abc(struct nmm_amino_table const *tbl);
+double nmm_amino_table_lprob(struct nmm_amino_table const *tbl, char const amino);
 
 /* Base abc */
-struct nmm_base_abc const *nmm_base_abc_create(struct imm_abc const *abc);
-void                       nmm_base_abc_destroy(struct nmm_base_abc const *base);
-struct imm_abc const *     nmm_base_abc_cast(struct nmm_base_abc const *base);
+struct nmm_base_abc const *nmm_base_abc_create(char const *symbols, char const any_symbol);
+struct nmm_base_abc const *nmm_base_abc_derived(struct imm_abc const *abc);
+void                       nmm_base_abc_destroy(struct nmm_base_abc const *base_abc);
+struct imm_abc const *     nmm_base_abc_super(struct nmm_base_abc const *base_abc);
 
 /* Base table */
-struct nmm_base_table const *nmm_base_table_create(struct nmm_base_abc const *base_abc, double a,
-                                                   double b, double c, double d);
-double                     nmm_base_table_lprob(struct nmm_base_table const *baset, char base);
-void                       nmm_base_table_destroy(struct nmm_base_table const *baset);
-struct nmm_base_abc const *nmm_base_table_get_base_abc(struct nmm_base_table const *baset);
+struct nmm_base_abc const *  nmm_base_table_abc(struct nmm_base_table const *baset);
+struct nmm_base_table const *nmm_base_table_create(struct nmm_base_abc const *abc, double a, double b,
+                                                   double c, double d);
+void                         nmm_base_table_destroy(struct nmm_base_table const *baset);
+double nmm_base_table_lprob(struct nmm_base_table const *baset, char const base);
 
 /* Codon */
+struct nmm_base_abc const *nmm_codon_abc(struct nmm_codon const *codon);
 struct nmm_codon *         nmm_codon_create(struct nmm_base_abc const *base);
 void                       nmm_codon_destroy(struct nmm_codon const *codon);
-struct nmm_base_abc const *nmm_codon_get_base(struct nmm_codon const *codon);
-int                        nmm_codon_set_triplet(struct nmm_codon *codon, struct nmm_triplet triplet);
 struct nmm_triplet         nmm_codon_get_triplet(struct nmm_codon const *codon);
-
-/* Codon table */
-struct nmm_codon_table const *nmm_codon_table_create(struct nmm_codon_lprob const *codonp);
-double nmm_codon_table_lprob(struct nmm_codon_table const *codont, struct nmm_codon const *codon);
-void   nmm_codon_table_destroy(struct nmm_codon_table const *codont);
-struct nmm_base_abc const *nmm_codon_table_get_base(struct nmm_codon_table const *codont);
+void                       nmm_codon_init(struct nmm_codon *codon, struct nmm_base_abc const *base);
+int                        nmm_codon_set_triplet(struct nmm_codon *codon, struct nmm_triplet triplet);
 
 /* Codon probability */
-struct nmm_codon_lprob *nmm_codon_lprob_create(struct nmm_base_abc const *base);
-int nmm_codon_lprob_set(struct nmm_codon_lprob *codonp, struct nmm_codon const *codon, double lprob);
+struct nmm_base_abc const *nmm_codon_lprob_abc(struct nmm_codon_lprob const *codonp);
+struct nmm_codon_lprob *   nmm_codon_lprob_create(struct nmm_base_abc const *abc);
+void                       nmm_codon_lprob_destroy(struct nmm_codon_lprob const *codonp);
 double nmm_codon_lprob_get(struct nmm_codon_lprob const *codonp, struct nmm_codon const *codon);
 int    nmm_codon_lprob_normalize(struct nmm_codon_lprob *codonp);
-void   nmm_codon_lprob_destroy(struct nmm_codon_lprob const *codonp);
-struct nmm_base_abc const *nmm_codon_lprob_get_base_abc(struct nmm_codon_lprob const *codonp);
+int nmm_codon_lprob_set(struct nmm_codon_lprob *codonp, struct nmm_codon const *codon, double lprob);
+
+/* Codon state */
+struct nmm_codon_state const *nmm_codon_state_create(char const *                  name,
+                                                     struct nmm_codon_lprob const *codonp);
+struct nmm_codon_state const *nmm_codon_state_derived(struct imm_state const *state);
+void                          nmm_codon_state_destroy(struct nmm_codon_state const *state);
+struct imm_state const *      nmm_codon_state_read(FILE *stream, struct nmm_model const *model);
+struct imm_state const *      nmm_codon_state_super(struct nmm_codon_state const *state);
+int nmm_codon_state_write(struct imm_state const *state, struct nmm_model const *model, FILE *stream);
+
+/* Codon table */
+struct nmm_base_abc const *   nmm_codon_table_abc(struct nmm_codon_table const *codont);
+struct nmm_codon_table const *nmm_codon_table_create(struct nmm_codon_lprob const *prob);
+void                          nmm_codon_table_destroy(struct nmm_codon_table const *codont);
+double nmm_codon_table_lprob(struct nmm_codon_table const *codont, struct nmm_codon const *codon);
 
 /* Frame state */
 struct nmm_frame_state const *nmm_frame_state_create(char const *                  name,
                                                      struct nmm_base_table const * baset,
                                                      struct nmm_codon_table const *codont,
                                                      double                        epsilon);
-double nmm_frame_state_lposterior(struct nmm_frame_state const *state, struct nmm_codon const *codon,
-                                  struct imm_seq const *seq);
 double nmm_frame_state_decode(struct nmm_frame_state const *state, struct imm_seq const *seq,
                               struct nmm_codon *codon);
-void   nmm_frame_state_destroy(struct nmm_frame_state const *state);
+struct nmm_frame_state const *nmm_frame_state_derived(struct imm_state const *state);
+void                          nmm_frame_state_destroy(struct nmm_frame_state const *state);
+double nmm_frame_state_lposterior(struct nmm_frame_state const *state, struct nmm_codon const *codon,
+                                  struct imm_seq const *seq);
+struct imm_state const *nmm_frame_state_read(FILE *stream, struct nmm_model const *model);
+struct imm_state const *nmm_frame_state_super(struct nmm_frame_state const *state);
+int nmm_frame_state_write(struct imm_state const *state, struct nmm_model const *model, FILE *stream);
+
+/* Input */
+struct nmm_input *      nmm_input_create(char const *filepath);
+int                     nmm_input_destroy(struct nmm_input const *input);
+bool                    nmm_input_eof(struct nmm_input const *input);
+struct nmm_model const *nmm_input_read(struct nmm_input *input);
+
+/* Model */
+struct imm_abc const *        nmm_model_abc(struct nmm_model const *model);
+struct nmm_base_table const * nmm_model_base_table(struct nmm_model const *model, uint32_t index);
+struct nmm_codon_lprob const *nmm_model_codon_lprob(struct nmm_model const *model, uint32_t index);
+struct nmm_codon_table const *nmm_model_codon_table(struct nmm_model const *model, uint32_t index);
+struct nmm_model const *      nmm_model_create(struct imm_hmm *hmm, struct imm_dp const *dp);
+void                          nmm_model_destroy(struct nmm_model const *model);
+uint32_t                      nmm_model_nbase_tables(struct nmm_model const *model);
+uint32_t                      nmm_model_ncodon_lprobs(struct nmm_model const *model);
+uint32_t                      nmm_model_ncodon_tables(struct nmm_model const *model);
+struct imm_hmm *              nmm_model_hmm(struct nmm_model const *model);
+struct imm_dp const *         nmm_model_dp(struct nmm_model const *model);
+struct imm_state const *      nmm_model_state(struct nmm_model const *model, uint32_t i);
+uint32_t                      nmm_model_nstates(struct nmm_model const *model);
+struct nmm_model const *      nmm_model_read(FILE *stream);
+int                           nmm_model_write(struct nmm_model const *io, FILE *stream);
+
+/* Output */
+struct nmm_output *nmm_output_create(char const *filepath);
+int                nmm_output_destroy(struct nmm_output const *output);
+int                nmm_output_write(struct nmm_output *output, struct nmm_model const *model);
+
+/* Triplet */
+struct nmm_triplet NMM_TRIPLET(char a, char b, char c);
